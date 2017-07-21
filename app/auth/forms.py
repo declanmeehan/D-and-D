2fcc243287b8f702dc69259db8149df303d408ab
Tim <bullzaye007@gmail.com>
2017-07-20 19:04:17 -0500

    dont remember; tired as fuck

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,10 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import Required, Email, Length, EqualTo
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[Required()])
     password = PasswordField("Password", validators=[Required()])
-
-    def validate_password(self, field):
-        pass
+    remember_me = BooleanField("Stay logged in")
+    submit = SubmitField("Log In")
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,2 +1,11 @@
 from flask import render_template, redirect, url_for
 from flask_login import login_required
+
+@auth.route("/login")
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        
+
+    return render_template("auth/login.html", form=form)
